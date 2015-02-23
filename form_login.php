f795af7e0f16b0c4bfa6c7e91a1a7c2ae307aa51
staichock <staichock@gmail.com>
2015-02-23 12:36:56 -0300

    formulario login

---
+++ form_login.php
@@ -0,0 +1,7 @@
+<?php
+?>
+	<form action="login.php" method="POST">
+		Login: 	<input type="text" name="login" />
+		Senha: 	<input type="password" name="senha" />
+				<input type="submit" value="Entrar" >
+	</form>	
