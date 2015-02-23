aa18dee20c06351af9e623cebd3a7d53cf015d8e
staichock <staichock@gmail.com>
2015-02-23 12:46:40 -0300

    div

--- form_login.php
+++ form_login.php
@@ -1,7 +1,11 @@
-<?php
-?>
-	<form action="login.php" method="POST">
-		Login: 	<input type="text" name="login" />
-		Senha: 	<input type="password" name="senha" />
-				<input type="submit" value="Entrar" >
-	</form>	
+<html>
+	<body>
+		<div id="form_login">
+			<form action="login.php" method="POST">
+				Login: 	<input type="text" name="login" />
+				Senha: 	<input type="password" name="senha" />
+						<input type="submit" value="Entrar" >
+			</form>
+		</div>
+	</body>
+</html>	
