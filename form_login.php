57b141240f8ff3defb4e8c197afafdb362b71a99
staichock <staichock@gmail.com>
2015-02-23 16:00:07 -0300

    Update form_login.php

--- form_login.php
+++ form_login.php
@@ -1,15 +1,15 @@
 <html>
 
 <head>
-	<link rel="stylesheet" type="text/css" href="stylos/estilo.css">
+        <link rel="stylesheet" type="text/css" href="stylos/estilo.css">
 </head>
-	<body>
-		<div id="form_login">
-			<form action="login.php" method="POST">
-				Login: 	<input type="text" name="login" />
-				Senha: 	<input type="password" name="senha" />
-						<input type="submit" value="Entrar" >
-			</form>
-		</div>
-	</body>
-</html>	
+        <body>
+                <div id="form_login">
+                        <form action="login.php" method="POST">
+                                <label class="lg"> Login:</label><input type="text" name="login" />
+                                Senha:  <input type="password" name="senha" />
+                                                <input class="botao" type="submit" value="Entrar" >
+                        </form>
+                </div>
+        </body>
+</html>
