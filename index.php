a6fd5b85a586ef506689894a77c1276ce1172645
staichock <staichock@gmail.com>
2015-02-26 15:27:48 -0300

    alteracoes

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
 <?php
+include("seguranca.php"); // Inclui o arquivo com o sistema de segurança
+	protegePagina(); // Chama a função que protege a página
+
+print("Estamos em desenvolvimento123!")
+?>
+
+<!-- <form action="form.php" method="POST">
+	Nome: <input type="text" name="nome" />
+	CPF: <input type="text" name="cpf" />
+</form>
+
+-->
